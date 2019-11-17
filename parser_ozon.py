7484301ea6648944241ab2f0c54778bcdf953b85
saper663_home <cheblin@yandex.ru>
2019-11-17 21:01:27 +0500

    fixed parser_ozon - del while loop

--- parser_ozon.py
+++ parser_ozon.py
@@ -1,39 +1,43 @@
 from bs4 import BeautifulSoup
 from time import sleep
-from price_bot import send_message
+from parser_petshop import get_html
 import requests
 
 
 
-url = 'https://www.ozon.ru/context/detail/id/33471742/'
-r = requests.get(url)
+url_ozon = 'https://www.ozon.ru/context/detail/id/33471742/'
+url_ozon_12 = 'https://www.ozon.ru/context/detail/id/33471728/'
+r_ozon = requests.get(url_ozon)
+r_ozon_12 = requests.get(url_ozon_12)
 
-def data_extraction_from_html(html_doc): 
-    soup = BeautifulSoup(html_doc, features='lxml')
+attrs_ozon = {'class':'a4l1'}
+                          
+
+def data_extraction_from_html(url, dict_attr): 
+    soup = BeautifulSoup(get_html(url), features='lxml')
     try:
-        return soup.find(attrs={'class':'a4l1'})
+        return soup.find(attrs=dict_attr)
     except TypeError:
         return None
 
 
-def mining_nunber(data_string):#перебирает по символьно строку и оставляет только цифры
+def mining_nunber(data_string): #перебирает по символьно строку и оставляет только цифры
     result = ''
     for char in data_string:
         if char.isdigit():
             result += char
     return int(result)
 
-def receiving_price():
-    html_text = data_extraction_from_html(r.text)
+def receiving_price(request_objc, attr):
+    html_text = data_extraction_from_html(request_objc.text, attr)
     price = mining_nunber(html_text.get_text())
     return price
 
-if __name__ == '__main__':
-    price = 0
-    while True:
-        new_price = receiving_price()
-        if new_price != price:
-            price = new_price
-            send_message(f'На озон - {price}')
-        sleep(600)
-    
+def price_ozon_18():
+    price = receiving_price(r_ozon, attrs_ozon)
+    return price
+
+def price_ozon_12():
+    price = receiving_price(r_ozon_12, attrs_ozon)
+    return price
+
