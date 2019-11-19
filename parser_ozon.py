6732f2696f64744cd90f03d121d367bbdaa371f4
saper663_home <cheblin@yandex.ru>
2019-11-19 21:28:58 +0500

    fixed condition parser in both files

--- parser_ozon.py
+++ parser_ozon.py
@@ -1,43 +1,20 @@
 from bs4 import BeautifulSoup
 from time import sleep
-from parser_petshop import get_html
+from parser_petshop import get_html, data_extraction_from_html, receiving_price, mining_nunber
 import requests
+import re
 
 
 
 url_ozon = 'https://www.ozon.ru/context/detail/id/33471742/'
-url_ozon_12 = 'https://www.ozon.ru/context/detail/id/33471728/'
-r_ozon = requests.get(url_ozon)
-r_ozon_12 = requests.get(url_ozon_12)
-
-attrs_ozon = {'class':'a4l1'}
+funtion = lambda tag: tag.name == 'span' and len(tag.attrs) == 2
                           
 
-def data_extraction_from_html(url, dict_attr): 
-    soup = BeautifulSoup(get_html(url), features='lxml')
-    try:
-        return soup.find(attrs=dict_attr)
-    except TypeError:
-        return None
-
-
-def mining_nunber(data_string): #перебирает по символьно строку и оставляет только цифры
-    result = ''
-    for char in data_string:
-        if char.isdigit():
-            result += char
-    return int(result)
-
-def receiving_price(request_objc, attr):
-    html_text = data_extraction_from_html(request_objc.text, attr)
-    price = mining_nunber(html_text.get_text())
-    return price
-
 def price_ozon_18():
-    price = receiving_price(r_ozon, attrs_ozon)
+    price = receiving_price(funtion, url_ozon, 7)
     return price
 
 def price_ozon_12():
-    price = receiving_price(r_ozon_12, attrs_ozon)
+    price = receiving_price(funtion, url_ozon, 6)
     return price
 
--- parser_petshop.py
+++ parser_petshop.py
@@ -7,17 +7,17 @@ import re
 
 url_petshop = 'https://www.petshop.ru/catalog/dogs/syxoi/vzroslye_1_6let/bessonovoy-dog-food-salmon-and-potato-bowl-lick/'
 r_petshop = requests.get(url_petshop)
-#attrs_petshop = {'class':'j_offer_price product-price'}
+conditions_for_petshop = lambda tag: tag.name == 'span' and not tag.attrs
+
 
 def get_html(url):
     html = requests.get(url).text
     return html
 
 
-def data_extraction_from_html(possition): 
-    
-    soup = BeautifulSoup(get_html(url_petshop), features='lxml')
-    text = str(soup.findAll(lambda tag: tag.name == 'span' and not tag.attrs ))
+def data_extraction_from_html(func,url, possition):     
+    soup = BeautifulSoup(get_html(url), features='lxml')
+    text = str(soup.findAll(func))
     result = re.findall(r'\d.\d{3}', text)
     return result[possition]
 
@@ -29,16 +29,15 @@ def mining_nunber(data_string): #перебирает по символьно с
             result += char
     return int(result)
 
-def receiving_price(possition):
-    price_string = data_extraction_from_html(possition)
+def receiving_price(func, url, possition):
+    price_string = data_extraction_from_html(func, url, possition)
     price = mining_nunber(price_string)
     return price
 
 def price_petshop_18():
-    price = receiving_price(0)
+    price = receiving_price(conditions_for_petshop, url_petshop, 0)
     return price
 
 def price_petshop_12():
-    price = receiving_price(1)
+    price = receiving_price(conditions_for_petshop, url_petshop, 1)
     return price
-
