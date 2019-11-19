31540649876916f95d4b1c8fe72195623c672ebd
saper663_home <cheblin@yandex.ru>
2019-11-19 21:34:29 +0500

    add start.py

--- start.py
+++ start.py
@@ -9,25 +9,25 @@ import requests
 
 
 if __name__ == '__main__':
-    price_ozon_12 = 0
-    price_ozon_18
-    price_petshop_12 = 0
-    price_petshop_18
+    price_ozon_12_digit = 0
+    price_ozon_18_digit = 0
+    price_petshop_12_digit = 0
+    price_petshop_18_digit = 0
     while True:
         new_price_ozon_12 = price_ozon_12()
         new_price_ozon_18 = price_ozon_18()
         new_price_petshop_12 = price_petshop_12()
         new_price_petshop_18 = price_petshop_18()
         if new_price_ozon_12 != price_ozon_12:
-            price_ozon_12 = new_price_ozon_12
-            send_message(f'На озон за 12кг - {price_ozon_12}')
+            price_ozon_12_digit = new_price_ozon_12
+            send_message(f'На озон за 12кг - {price_ozon_12_digit}')
         if new_price_ozon_18 != price_ozon_18:
-            price_ozon_18 = new_price_ozon_18
-            send_message(f'На озон за 18кг - {price_ozon_18}')
+            price_ozon_18_digit = new_price_ozon_18
+            send_message(f'На озон за 18кг - {price_ozon_18_digit}')
         if new_price_petshop_12 != price_petshop_12:
-            price_petshop_12 = new_price_petshop_12
-            send_message(f'На petshop за 12кг - {price_petshop_12}')
+            price_petshop_12_digit = new_price_petshop_12
+            send_message(f'На petshop за 12кг - {price_petshop_12_digit}')
         if new_price_petshop_18 != price_petshop_18:
-            price_petshop_18 = new_price_petshop_18
-            send_message(f'На petshop за 18кг - {price_petshop_18}')
+            price_petshop_18_digit = new_price_petshop_18
+            send_message(f'На petshop за 18кг - {price_petshop_18_digit}')
         sleep(600)
