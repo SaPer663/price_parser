9d87e8baecd971bad66144b5c062c7bd2a2a024e
saper663_home <cheblin@yandex.ru>
2019-11-24 21:20:01 +0500

    fixed parser bug add func: by_id and by_ weight

--- parser_petshop.py
+++ parser_petshop.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
-#from time import sleep
-#from price_bot import send_message
 import requests
 import re
 
 
 url_petshop = 'https://www.petshop.ru/catalog/dogs/syxoi/vzroslye_1_6let/bessonovoy-dog-food-salmon-and-potato-bowl-lick/'
-r_petshop = requests.get(url_petshop)
+id_18 = 'offer-63133'
+id_12 = 'offer-63132'
 conditions_for_petshop = lambda tag: tag.name == 'span' and not tag.attrs
 
 
@@ -15,11 +14,19 @@ def get_html(url):
     return html
 
 
-def data_extraction_from_html(func,url, possition):     
-    soup = BeautifulSoup(get_html(url), features='lxml')
+def data_extraction_from_html_by_id(html_doc, id): 
+    soup = BeautifulSoup(html_doc, features='lxml')
+    try:
+        return soup.find(attrs={'id': id})
+    except TypeError:
+        return None
+
+
+def extraction_price_by_weight(func, url, id):     
+    soup = data_extraction_from_html_by_id(get_html(url), id)
     text = str(soup.findAll(func))
     result = re.findall(r'\d.\d{3}', text)
-    return result[possition]
+    return result[0]
 
 
 def mining_nunber(data_string): #перебирает по символьно строку и оставляет только цифры
@@ -29,15 +36,15 @@ def mining_nunber(data_string): #перебирает по символьно с
             result += char
     return int(result)
 
-def receiving_price(func, url, possition):
-    price_string = data_extraction_from_html(func, url, possition)
+def receiving_price(func, url, id):
+    price_string = extraction_price_by_weight(func, url, id)
     price = mining_nunber(price_string)
     return price
 
 def price_petshop_18():
-    price = receiving_price(conditions_for_petshop, url_petshop, 0)
+    price = receiving_price(conditions_for_petshop, url_petshop, id_18)
     return price
 
 def price_petshop_12():
-    price = receiving_price(conditions_for_petshop, url_petshop, 1)
+    price = receiving_price(conditions_for_petshop, url_petshop, id_12)
     return price
