09c949d5fb122416bd2b7c4ee8447885011a73b8
saper663_home <cheblin@yandex.ru>
2019-11-24 20:10:04 +0500

    fixed bug print

--- start.py
+++ start.py
@@ -19,16 +19,16 @@ if __name__ == '__main__':
         new_price_ozon_18 = price_ozon_18()
         new_price_petshop_12 = price_petshop_12()
         new_price_petshop_18 = price_petshop_18()
-        if new_price_ozon_12 != price_ozon_12:
+        if new_price_ozon_12 != price_ozon_12_digit:
             price_ozon_12_digit = new_price_ozon_12
             send_message(f'На озон за 12кг - {price_ozon_12_digit}')
-        if new_price_ozon_18 != price_ozon_18:
+        if new_price_ozon_18 != price_ozon_18_digit:
             price_ozon_18_digit = new_price_ozon_18
             send_message(f'На озон за 18кг - {price_ozon_18_digit}')
-        if new_price_petshop_12 != price_petshop_12:
+        if new_price_petshop_12 != price_petshop_12_digit:
             price_petshop_12_digit = new_price_petshop_12
             send_message(f'На petshop за 12кг - {price_petshop_12_digit}')
-        if new_price_petshop_18 != price_petshop_18:
+        if new_price_petshop_18 != price_petshop_18_digit:
             price_petshop_18_digit = new_price_petshop_18
             send_message(f'На petshop за 18кг - {price_petshop_18_digit}')
         sleep(600)
