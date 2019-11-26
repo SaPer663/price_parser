02cfbb7e3ad10597d101db4111d8c03146813343
saper663_home <cheblin@yandex.ru>
2019-11-26 21:37:09 +0500

    del price_digit and add str(check)

--- start.py
+++ start.py
@@ -15,29 +15,26 @@ if __name__ == '__main__':
     data_base_price = {}
     with open(path, 'r') as db:
         data_base_price = json.load(db)
-    price_ozon_12_digit = data_base_price['o_12']
-    price_ozon_18_digit = data_base_price['o_18']
-    price_petshop_12_digit = data_base_price['p_12']
-    price_petshop_18_digit = data_base_price['p_18']
+    
     while True:
-        check_db = data_base_price
+        check_db = str(data_base_price)
         new_price_ozon_12 = price_ozon_12()
         new_price_ozon_18 = price_ozon_18()
         new_price_petshop_12 = price_petshop_12()
         new_price_petshop_18 = price_petshop_18()
-        if new_price_ozon_12 != price_ozon_12_digit:
+        if new_price_ozon_12 != data_base_price['o_12']:
             data_base_price['o_12'] = new_price_ozon_12
-            send_message(f'На озон за 12кг - {data_base_price['o_12']}')
-        if new_price_ozon_18 != price_ozon_18_digit:
+            send_message(f"На озон за 12кг - {data_base_price['o_12']}")
+        if new_price_ozon_18 != data_base_price['o_18']:
             data_base_price['o_18'] = new_price_ozon_18
-            send_message(f'На озон за 18кг - {data_base_price['o_18']}')
-        if new_price_petshop_12 != price_petshop_12_digit:
+            send_message(f"На озон за 18кг - {data_base_price['o_18']}")
+        if new_price_petshop_12 != data_base_price['p_12']:
             data_base_price['p_12'] = new_price_petshop_12
-            send_message(f'На petshop за 12кг - {data_base_price['p_12']}')
-        if new_price_petshop_18 != price_petshop_18_digit:
+            send_message(f"На petshop за 12кг - {data_base_price['p_12']}")
+        if new_price_petshop_18 != data_base_price['p_18']:
             data_base_price['p_18'] = new_price_petshop_18
-            send_message(f'На petshop за 18кг - {data_base_price['p_18']}')
-        if check_db != data_base_price:
+            send_message(f"На petshop за 18кг - {data_base_price['p_18']}")
+        if check_db != str(data_base_price):
             with open(path, 'w') as db:
                 json.dump(data_base_price, db)
         sleep(600)
