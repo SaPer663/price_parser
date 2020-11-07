baab7f6878865b8709a9565e420f48d2e4039132
saper663_home <saper663ru@gmail.com>
2020-11-07 14:06:31 +0500

    Added the price_petshop function

--- parser_ozon.py
+++ parser_ozon.py
@@ -1,21 +1,24 @@
-from bs4 import BeautifulSoup
-from time import sleep
-from config import url_ozon, url_ozon_of_get_json
 import requests
 import re
 import json
+from bs4 import BeautifulSoup
+from time import sleep
+from config import url_ozon, url_ozon_of_get_json, url_petshop, url_petshop_of_get_json
 
 
-def get_json_from_server():     
-    s = requests.Session()
-    s.get(url_ozon)   
-    sleep(2)
-    return s.get(url_ozon_of_get_json).json()
+def get_json_from_server(url_base, url_json):
+    try:
+        s = requests.Session()
+        s.get(url_base)
+        sleep(2)
+        return s.get(url_json).json()
+    except Exception as ex:
+        return ex
 
 
-def price_ozon_12_18(price_12, price_18):
-    result = [price_12, price_18]
-    json_input = get_json_from_server()
+def price_ozon_12_18():
+    result = [0, 0]
+    json_input = get_json_from_server(url_ozon, url_ozon_of_get_json)
     list_prices_and_titles = json_input['pdp']['product']['product-220660-default-1']\
                                        ['readyAspects']['regular']['regularVariants']\
                                        [1]['aspectVariants']
@@ -26,3 +29,22 @@ def price_ozon_12_18(price_12, price_18):
             result[1] = price['price']['totalPrice']
 
     return result
+
+def price_petshop_12_18():
+    result = {'price_12': 0, 'price_18': 0}
+    list_prices_and_titles = get_json_from_server(url_petshop, url_petshop_of_get_json)
+    if isinstance(list_prices_and_titles, list):
+        for price in list_prices_and_titles:
+            if int(price['Size']) == 12000:
+                if price['IsAvailable']:
+                    result['price_12'] = price['Price']
+                else:
+                    result['price_12'] = -1
+            if int(price['Size']) == 18000:
+                if price['IsAvailable']:
+                    result['price_18'] = price['Price']
+                else:
+                    result['price_18'] = -1
+        return result.values()
+    else:
+        return list_prices_and_titles
