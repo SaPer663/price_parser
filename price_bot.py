0e6cba84862fa48b192f2c52ab47192c41bc77c7
saper663_home <saper663ru@gmail.com>
2020-11-07 14:15:11 +0500

    added a price function that displays prices

--- price_bot.py
+++ price_bot.py
@@ -4,6 +4,7 @@
 import cherrypy
 import telebot
 from config import token, chat_id, path_to_cert, url_server
+from json_db import read_file
 
 
 BOT_TOKEN = token
@@ -15,6 +16,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=["start"])
 def command_start(message):
     bot.send_message(message.chat.id, "Привет! Я price_bot")
+
+@bot.message_handler(commands=["price"])
+def command_price(message):
+    load_price = read_file()
+    price_12 = load_price['p_12']
+    price_18 = load_price['p_18']
+    bot.send_message(message.chat.id, f'На petshop\n за 12кг - {price_12}\n На petshop за 18кг - {price_18}')
     
 
 def send_message(text, id=Chat_id):
