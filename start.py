4dcb82c5ffd8f53d9b4c8dd6474ecc63da072911
saper663_home <saper663ru@gmail.com>
2020-11-07 14:10:15 +0500

    Removed the price_ozon function and added database access functions from json_db.py

--- start.py
+++ start.py
@@ -1,40 +1,31 @@
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
-from parser_petshop import get_html
 from price_bot import send_message
-from parser_ozon import price_ozon_12_18
-from parser_petshop import price_petshop_12, price_petshop_18
+from parser_ozon import price_petshop_12_18
 from config import path
-import requests
-import json
+from json_db import read_file, write_file
 
 
 
 if __name__ == '__main__':
     print('working')
     data_base_price = {}
-    with open(path, 'r') as db:
-        data_base_price = json.load(db)
+    data_base_price = read_file()
     
     while True:
-        check_db = str(data_base_price)
-        new_price_ozon_12, new_price_ozon_18 = price_ozon_12_18(data_base_price['o_12'],\
-                                                                data_base_price['o_18'])
-        new_price_petshop_12 = price_petshop_12()
-        new_price_petshop_18 = price_petshop_18()
-        if new_price_ozon_12 != data_base_price['o_12']:
-            data_base_price['o_12'] = new_price_ozon_12
-            send_message(f"На озон за 12кг - {data_base_price['o_12']}")
-        if new_price_ozon_18 != data_base_price['o_18']:
-            data_base_price['o_18'] = new_price_ozon_18
-            send_message(f"На озон за 18кг - {data_base_price['o_18']}")
-        if new_price_petshop_12 != data_base_price['p_12']:
-            data_base_price['p_12'] = new_price_petshop_12
-            send_message(f"На petshop за 12кг - {data_base_price['p_12']}")
-        if new_price_petshop_18 != data_base_price['p_18']:
-            data_base_price['p_18'] = new_price_petshop_18
-            send_message(f"На petshop за 18кг - {data_base_price['p_18']}")
-        if check_db != str(data_base_price):
-            with open(path, 'w') as db:
-                json.dump(data_base_price, db)
+        if isinstance(data_base_price, dict):
+            check_db = str(data_base_price)
+            new_price_petshop_12, new_price_petshop_18 = price_petshop_12_18()
+            
+            if new_price_petshop_12 != data_base_price['p_12']:
+                data_base_price['p_12'] = new_price_petshop_12
+                send_message(f"На petshop за 12кг - {data_base_price['p_12']}")
+            if new_price_petshop_18 != data_base_price['p_18']:
+                data_base_price['p_18'] = new_price_petshop_18
+                send_message(f"На petshop за 18кг - {data_base_price['p_18']}")
+            if check_db != str(data_base_price):
+                write_file(data_base_price)
+        else:
+            send_message(f'У нас ошибка {data_base_price}')
         sleep(600)
